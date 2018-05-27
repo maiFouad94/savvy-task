c73cca6fc45fb46e7aabe22bcb6cde9635f4e538
maiFouad94 <mai_elrefaey@yahoo.com>
2018-05-27 12:42:37 +0200

    tasks routes

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('tasks','ItemsController@index');
 
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+Route::get('/home', 'HomeController@index')->name('home');
